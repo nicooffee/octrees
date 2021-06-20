f8d651c1c671c339244df4043049b7a1c16eb8f9
Nicolas Honorato <nicolas.honorato@usach.cl>
2021-06-20 15:35:26 -0400

    pruebas added

---
+++ pruebas.py
@@ -0,0 +1,10 @@
+from octrees import Octree, octree_from_list
+from geometry import *
+
+o = Octree(((0.0, 1.0), (0.0, 1.0), (0.0, 1.0)))
+o.insert((0.33, 0.66, 0.99), 1)
+o.insert((0.12, 0.34, 0.56), 2)
+o.insert((0.98, 0.76, 0.54), 3)
+o2 = o.deform(lambda p: (p[0]+0.1,p[1],p[2]))
+for p in o2:
+    print(p)
